dda953a99adc261fe7bd34c9decc41d71f988209
Aleksey Shikhov <shikhov@gmail.com>
2024-11-01 18:19:46 +0500

    merge with master

--- app/src/app.py
+++ app/src/app.py
@@ -1,66 +1,116 @@
 import logging
 import re
 import asyncio
+from datetime import datetime
 
 from aiogram import Bot, Dispatcher, Router, F, types
 from aiogram.exceptions import TelegramBadRequest
 from aiogram.filters.chat_member_updated import ChatMemberUpdatedFilter, JOIN_TRANSITION
+from aiogram.utils.text_decorations import html_decoration as hd
 from pymongo import MongoClient
 
 from config import CONNSTRING, DBNAME
 db = MongoClient(CONNSTRING).get_database(DBNAME)
 
 
-def processRegexList(regex_list):
-    subs = {
-        'а': 'a',
-        'к': 'k',
-        'и': 'u',
-        'р': 'p',
-        'о': 'o0',
-        'е': 'ёe',
-        'т': 't',
-        'с': 'c',
-        'н': 'h',
-        'в': 'b',
-        'з': '3',
-        'у': 'y',
-        'х': 'x'
-    }
-    out_list = []
-    for regex in regex_list:
-        out_regex = ''
-        for char in regex:
-            if char in subs:
-                out_regex += '[' + char + subs[char] + ']'
-            else:
-                out_regex += char
-        out_list.append(out_regex)
+class RegexChecker:
+    def __init__(self) -> None:
+        self.rlist = {}
+        self.matched_regex = None
 
-    return out_list
+    def load_list(self, regex_list):
+        SUBS = {
+            'а': 'a',
+            'к': 'k',
+            'и': 'u',
+            'р': 'p',
+            'о': 'o0',
+            'е': 'ёe',
+            'т': 't',
+            'с': 'c',
+            'н': 'h',
+            'в': 'b',
+            'з': '3',
+            'у': 'y',
+            'х': 'x'
+        }
+        tmp = {}
+        stat = db.settings.find_one({'_id': 'stat'})
+
+        for regex in regex_list:
+            out_regex = ''
+            for char in regex:
+                if char in SUBS:
+                    out_regex += '[' + char + SUBS[char] + ']'
+                else:
+                    out_regex += char
+            tmp[regex] = {
+                'regex': out_regex,
+                'count': stat['regex'].get(regex, 0)
+            }
+            if regex.startswith(r'[\u'):
+                tmp[regex]['flags'] = re.IGNORECASE + re.ASCII
+
+        self.rlist = dict(sorted(tmp.items(), key=lambda item: item[1]['count'], reverse=True))
+
+    def check(self, text):
+        if not text: return False
+        for key, value in self.rlist.items():
+            regex = value['regex']
+            flags = value.get('flags', re.IGNORECASE + re.UNICODE)
+            if re.search(regex, text, flags):
+                self.matched_regex = key
+                return True
+
+        return False
+
+    def updateStat(self, stat):
+        if not self.matched_regex:
+            return
+        self.rlist[self.matched_regex]['count'] += 1
+        self.rlist = dict(sorted(self.rlist.items(), key=lambda item: item[1]['count'], reverse=True))
+        stat['regex'][self.matched_regex] = stat['regex'].get(self.matched_regex, 0) + 1
+        self.matched_regex = None
 
 
 def loadSettings():
-    global TOKEN, ADMINCHATID, REGEX_LIST, CHATS
+    global TOKEN, ADMINCHATID, CHATS
 
     settings = db.settings.find_one({'_id': 'settings'})
 
     TOKEN = settings['TOKEN']
     ADMINCHATID = settings['ADMINCHATID']
-    REGEX_LIST = processRegexList(settings['REGEX_LIST'])
-    CHATS = {}
-    for entry in settings['CHATS']:
-        for chat in entry['chats']:
-            CHATS[chat] = entry['admin']
+    regexChecker.load_list(settings['REGEX_LIST'])
+    CHATS = {chat['id']: chat['admin_id'] for chat in settings['CHATS']}
+
+
+def initServiceData():
+    stat_struct = {
+        'regex': {},
+        'daily': {}
+    }
+    stat = db.settings.find_one({'_id': 'stat'})
+    if stat:
+        for key, value in stat_struct.items():
+            stat[key] = stat.get(key, value)
+    else:
+        stat = stat_struct
+    db.settings.update_one({'_id': 'stat'}, {'$set': stat}, upsert=True)
+
+
+FORBIDDEN_ENTITIES = {'text_link', 'url', 'mention', 'custom_emoji'}
 
 # Configure logging
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s] %(message)s')
+
+regexChecker = RegexChecker()
+usersCache = {}
+
+# service data
+initServiceData()
 
 # settings
 loadSettings()
-
-# users cache
-usersCache = {}
 
 # Initialize bot and dispatcher
 bot = Bot(token=TOKEN, parse_mode='HTML')
@@ -68,29 +118,18 @@ dp = Dispatcher()
 router = Router()
 
 
-def checkRegex(text):
-    if not text: return False
-    for regex in REGEX_LIST:
-        flags = re.IGNORECASE + re.UNICODE
-        if regex.startswith(r'[\u'):
-            flags = re.IGNORECASE + re.ASCII
-
-        if re.search(regex, text, flags):
-            return True
-
-    return False
-
-
 def checkEntities(message: types.Message):
     entities = message.entities or message.caption_entities or []
     for entity in entities:
-        if entity.type in {'text_link', 'url', 'mention', 'custom_emoji'}:
+        if entity.type in FORBIDDEN_ENTITIES:
             return True
     return False
 
 
 def isUserLegal(message: types.Message):
-    key = str(message.chat.id) + '_' + str(message.from_user.id)
+    chat = message.chat
+    user = message.from_user
+    key = f'{chat.id}_{user.id}'
     if key in usersCache:
         return usersCache[key]
 
@@ -98,10 +137,10 @@ def isUserLegal(message: types.Message):
     if not doc:
         doc = {
                 '_id': key,
-                'first_name': message.from_user.first_name,
-                'last_name': message.from_user.last_name,
-                'username': message.from_user.username,
-                'chat_title': message.chat.title,
+                'first_name': user.first_name,
+                'last_name': user.last_name,
+                'username': user.username,
+                'chat_title': chat.title,
                 'islegal': True
             }
         db.users.insert_one(doc)
@@ -114,7 +153,7 @@ async def isChatAllowed(chat: types.Chat):
     if chat.id in CHATS: return True
     if chat.type == 'private': return True
 
-    logging.info(f'chat id {chat.id} is not allowed! Leaving chat')
+    logging.warning(f'chat id {chat.id} is not allowed! Leaving chat')
     try:
         await bot.leave_chat(chat.id)
     except Exception:
@@ -134,7 +173,7 @@ async def processJoin(event: types.ChatMemberUpdated):
 
     user = event.new_chat_member.user
     chat = event.chat
-    docid = str(chat.id) + '_' + str(user.id)
+    docid = f'{chat.id}_{user.id}'
     data = {
             '_id': docid,
             'first_name': user.first_name,
@@ -166,9 +205,10 @@ async def processCmdUnban(message: types.Message):
         return
     key = rg.group(1)
     (chat_id, user_id) = key.split('_')
-    if CHATS.get(chat_id) != message.chat.id:
+    if CHATS.get(int(chat_id)) != message.chat.id:
         await message.answer('⚠ You are not admin of this chat')
         return
+
     try:
         result = await bot.unban_chat_member(chat_id=chat_id, user_id=user_id, only_if_banned=True)
         if not result:
@@ -189,41 +229,64 @@ async def processCmdReload(message: types.Message):
     await message.answer('Settings sucessfully reloaded')
 
 
-@router.message(F.chat.type != 'private')
-async def processChatMsg(message: types.Message):
+async def checkForSpam(message: types.Message):
     chat = message.chat
     user = message.from_user
-    if not await isChatAllowed(chat): return
     admin_id = CHATS[chat.id]
-    if user.id == admin_id: return
-    if user.id == bot.id: return
+
+    if user.id == admin_id:
+        return False
+    if user.id == ADMINCHATID:
+        return False
+    if user.id == bot.id:
+        return False
 
     if message.sender_chat:
         await message.delete()
-        return
+        return True
 
     if isUserLegal(message):
-        return
+        return False
 
     text = message.text or message.caption
     if not (text or message.reply_markup):
-        return
+        return False
 
-    key = str(chat.id) + '_' + str(user.id)
+    key = f'{chat.id}_{user.id}'
 
-    if checkRegex(text) or message.reply_markup or checkEntities(message):
+    if message.reply_markup or checkEntities(message) or regexChecker.check(text):
         await bot.ban_chat_member(chat_id=chat.id, user_id=user.id)
         if not message.reply_markup:
             await message.forward(admin_id)
-            await bot.send_message(admin_id, '💩 Spam from user: ' + user.full_name + '\n' + key)
+            await bot.send_message(admin_id, f'💩 Spam from user: {hd.quote(user.full_name)}\n{key}')
 
         await message.delete()
         db.users.delete_one({'_id': key})
         usersCache.pop(key)
-    else:
-        db.users.update_one({'_id' : key}, {'$set': {'islegal': True}})
-        usersCache[key] = True
+        return True
 
+    db.users.update_one({'_id' : key}, {'$set': {'islegal': True}})
+    usersCache[key] = True
+    return False
+
+
+def updateStat(message: types.Message):
+    chat_id = str(message.chat.id)
+    stat = db.settings.find_one({'_id': 'stat'})
+    today = str(datetime.today().date())
+    stat['daily'][chat_id] = stat['daily'].get(chat_id, {})
+    stat['daily'][chat_id][today] = stat['daily'][chat_id].get(today, 0) + 1
+    regexChecker.updateStat(stat)
+    db.settings.update_one({'_id': 'stat'}, {'$set': stat})
+
+
+@router.message(F.chat.type != 'private')
+async def processMsg(message: types.Message):
+    if not await isChatAllowed(message.chat):
+        return
+
+    if await checkForSpam(message):
+        updateStat(message)
 
 async def main():
     dp.include_router(router)
