2a7742367338e1fd56c106d6d5cd63f89eff7d86
Aleksey Shikhov <shikhov@gmail.com>
2025-01-02 12:39:39 +0500

    refactor app.py by removing unused functions and variables, simplifying user legality checks, and enhancing message handling

--- app/src/app.py
+++ app/src/app.py
@@ -1,20 +1,15 @@
 import logging
-import re
 import asyncio
 import os
-from datetime import datetime
 import traceback
 
 from aiogram import Bot, Dispatcher, Router, F, types
-from aiogram.exceptions import TelegramBadRequest
-from aiogram.filters.chat_member_updated import ChatMemberUpdatedFilter, JOIN_TRANSITION
 from aiogram.utils.text_decorations import html_decoration as hd
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 from aiogram.utils.callback_answer import CallbackAnswerMiddleware
 import random
 from pymongo import MongoClient
 
-from regex_checker import RegexChecker
 import config
 
 connstring = config.CONNSTRING or os.getenv('connstring')
@@ -39,7 +34,6 @@ class Group:
         self.timeout_text = data.get('timeout_text', TIMEOUT_TEXT)
         self.captcha_timeout = data.get('captcha_timeout', CAPTCHA_TIMEOUT)
         self.logchatid = data.get('logchatid', LOGCHATID)
-        self.force_spamcheck = data.get('force_spamcheck', FORCE_SPAMCHECK)
         self.delete_joins = data.get('delete_joins', DELETE_JOINS)
         self.delete_anonymous = data.get('delete_anonymous', DELETE_ANONYMOUS)
 
@@ -77,12 +71,8 @@ def loadSettings():
         ERROR_TEXT = settings['ERROR_TEXT']
         TIMEOUT_TEXT = settings['TIMEOUT_TEXT']
         CAPTCHA_TIMEOUT = settings['CAPTCHA_TIMEOUT']
-        FORCE_SPAMCHECK = settings['FORCE_SPAMCHECK']
         DELETE_JOINS = settings['DELETE_JOINS']
         DELETE_ANONYMOUS = settings['DELETE_ANONYMOUS']
-
-        stat = db.settings.find_one({'_id': 'stat'})
-        regexChecker.load_list(settings['REGEX_LIST'], stat)
     except Exception:
         traceback.print_exc()
         return False
@@ -90,30 +80,8 @@ def loadSettings():
     return True
 
 
-def initServiceData():
-    stat_struct = {
-        'regex': {},
-        'daily': {}
-    }
-    stat = db.settings.find_one({'_id': 'stat'})
-    if stat:
-        for key, value in stat_struct.items():
-            stat[key] = stat.get(key, value)
-    else:
-        stat = stat_struct
-    db.settings.update_one({'_id': 'stat'}, {'$set': stat}, upsert=True)
-
-
-FORBIDDEN_ENTITIES = {'text_link', 'url', 'mention', 'custom_emoji'}
-
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s] %(message)s')
-
-regexChecker = RegexChecker()
-usersCache = {}
-
-# service data
-initServiceData()
 
 # settings
 if not loadSettings():
@@ -125,22 +93,9 @@ dp = Dispatcher()
 router = Router()
 
 
-def checkEntities(message: types.Message):
-    entities = message.entities or message.caption_entities or []
-    for entity in entities:
-        if entity.type in FORBIDDEN_ENTITIES:
-            return True
-    return False
-
-
 def isUserLegal(user: types.User, chat: types.Chat):
-    key = f'{chat.id}_{user.id}'
-    if key in usersCache:
-        return usersCache[key]
-
-    doc = db.users.find_one({'_id': key})
+    doc = db.users.find_one({'_id': f'{chat.id}_{user.id}'})
     if doc:
-        usersCache[key] = doc['islegal']
         return doc['islegal']
     return False
 
@@ -153,7 +108,7 @@ async def isChatAllowed(chat: types.Chat):
 
     logging.warning(f'chat id {chat.id} ({chat.title}) is not allowed! Leaving chat')
     try:
-        await bot.leave_chat(chat.id)
+        await chat.leave()
     except Exception:
         pass
     return False
@@ -164,7 +119,6 @@ async def outer_middleware(handler, event, data):
     chat = getattr(event.event, "chat", None)
     if chat and not await isChatAllowed(chat):
         return
-
     return await handler(event, data)
 
 
@@ -182,82 +136,12 @@ async def deleteJoinMessage(message: types.Message):
         logging.warning(f'Cannot delete join message in "{message.chat.title}" (no admin rights?)')
 
 
-@router.message((F.text.lower() == 'unban') & (F.chat.id == LOGCHATID))
-async def processCmdUnban(message: types.Message):
-    if not (message.reply_to_message and message.reply_to_message.text):
-        await message.answer('⚠ You must reply to message to use this command')
-        return
-    rg = re.search(r'\n(-?\d+_\d+)$', message.reply_to_message.text)
-    if not rg:
-        await message.answer('⚠ IDs not found in message')
-        return
-    key = rg.group(1)
-    (chat_id, user_id) = key.split('_')
-    try:
-        result = await bot.unban_chat_member(chat_id=chat_id, user_id=user_id, only_if_banned=True)
-        if not result:
-            await message.answer('⚠ User unban error')
-            return
-    except TelegramBadRequest as e:
-        await message.answer('⚠ ' + e.message)
-        return
-
-    db.users.insert_one({'_id': key, 'islegal': True})
-    usersCache[key] = True
-    await message.answer('✅ User unbanned successfully')
-
-
 @router.message((F.text == '/reload') & (F.chat.id == ADMINCHATID))
 async def processCmdReload(message: types.Message):
     if not loadSettings():
         await message.answer('Error!')
         return
-
     await message.answer('Settings sucessfully reloaded')
-
-
-async def checkForSpam(message: types.Message):
-    chat = message.chat
-    user = message.from_user
-
-    if user.id == ADMINCHATID:
-        return False
-    if user.id == bot.id:
-        return False
-
-    if isUserLegal(user, chat):
-        return False
-
-    text = message.text or message.caption
-    if not (text or message.reply_markup):
-        return False
-
-    key = f'{chat.id}_{user.id}'
-
-    if message.reply_markup or checkEntities(message) or regexChecker.check(text):
-        await bot.ban_chat_member(chat_id=chat.id, user_id=user.id)
-        if not message.reply_markup:
-            await message.forward(LOGCHATID)
-            await bot.send_message(LOGCHATID, f'{HASHTAG}\n💩 Spam from user: {hd.quote(user.full_name)}\n{key}')
-
-        await message.delete()
-        db.users.delete_one({'_id': key})
-        usersCache.pop(key)
-        return True
-
-    db.users.update_one({'_id' : key}, {'$set': {'islegal': True}})
-    usersCache[key] = True
-    return False
-
-
-def updateStat(message: types.Message):
-    chat_id = str(message.chat.id)
-    stat = db.settings.find_one({'_id': 'stat'})
-    today = str(datetime.today().date())
-    stat['daily'][chat_id] = stat['daily'].get(chat_id, {})
-    stat['daily'][chat_id][today] = stat['daily'][chat_id].get(today, 0) + 1
-    regexChecker.updateStat(stat)
-    db.settings.update_one({'_id': 'stat'}, {'$set': stat})
 
 
 @router.chat_join_request()
@@ -269,11 +153,11 @@ async def processJoinRequest(update: types.ChatJoinRequest):
         return
     group = Group(chat=chat)
     logname = hd.quote(f'{user.full_name} @{user.username}' if user.username else user.full_name)
-    builder = InlineKeyboardBuilder()
+    kb = InlineKeyboardBuilder()
     for emoji in group.random_emoji():
-        builder.button(text=emoji, callback_data=f'{emoji}#{chat.id}#{chat.username}')
-    builder.adjust(group.emoji_rowsize)
-    message = await bot.send_message(user.id, group.welcome_text, reply_markup=builder.as_markup())
+        kb.button(text=emoji, callback_data=f'{emoji}#{chat.id}#{chat.username}')
+    kb.adjust(group.emoji_rowsize)
+    message = await bot.send_message(user.id, group.welcome_text, reply_markup=kb.as_markup())
     await bot.send_message(group.logchatid, f'{HASHTAG}\n{logname} wants to join {chat.title}')
     await asyncio.sleep(group.captcha_timeout)
     try:
@@ -311,7 +195,6 @@ async def callbackHandler(query: types.CallbackQuery):
                 'islegal': True
             }
         db.users.update_one({'_id': docid}, {'$set': doc}, upsert=True)
-        usersCache[docid] = True
     else:
         await bot.edit_message_text(group.fail_text, user.id, msg_id)
         try:
@@ -328,10 +211,6 @@ async def processMsg(message: types.Message):
     group = Group(chat=message.chat)
     if message.sender_chat and group.delete_anonymous:
         await message.delete()
-
-    if group.force_spamcheck:
-        if await checkForSpam(message):
-            updateStat(message)
 
 
 async def main():
